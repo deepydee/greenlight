7f2522894800e45ddb03e453fec813a758078b09
Vitalii Semenov <v.semenov@astrio.net>
2025-05-16 17:39:07 +0500

    add global rate limiter middleware

--- src/cmd/api/middleware.go
+++ src/cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/time/rate"
 	"net/http"
 )
 
@@ -15,5 +16,20 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		}()
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(nexr http.Handler) http.Handler {
+	// Initialize a new rate limiter which allows an average of 2 requests per second,
+	// with a maximum of 4 requests in a single ‘burst’.
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		nexr.ServeHTTP(w, r)
 	})
 }
