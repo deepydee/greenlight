69726dcda6e7ba2a89a79b3a93727c0ae2a68e04
Vitalii Semenov <v.semenov@astrio.net>
2025-05-20 15:35:20 +0500

    implement graceful shutdown

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/deepydee/greenlight/src/internal/jsonlog"
 	"github.com/joho/godotenv"
 	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -86,21 +85,10 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-
-	logger.PrintInfo(fmt.Sprintf("starting %s server on %s...", cfg.env, srv.Addr), map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
