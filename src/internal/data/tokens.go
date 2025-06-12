193d6dea50febcd86110096ff4ea144c0e360bbd
Vitalii Semenov <v.semenov@astrio.net>
2025-06-12 21:15:31 +0500

    send activation tokens

--- src/internal/data/tokens.go
+++ src/internal/data/tokens.go
@@ -69,7 +69,7 @@ func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, 
 // Insert adds the data for a specific token to the tokens table.
 func (m TokenModel) Insert(token *Token) error {
 	query := `
-INSERT INTO tokens (hash, user_id, expiry, scope)
+INSERT INTO tokens (token_hash, user_id, expiry, scope)
 VALUES ($1, $2, $3, $4)`
 
 	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
