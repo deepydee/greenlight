134958b47e741467e3042b9c8b687fefc0e57d72
Vitalii Semenov <v.semenov@astrio.net>
2025-08-03 21:04:41 +0500

    add metrics

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"fmt"
 	"github.com/deepydee/greenlight/src/internal/data"
@@ -11,6 +12,7 @@ import (
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -104,6 +106,20 @@ func main() {
 
 	defer db.Close()
 	logger.PrintInfo("database connection pool established", nil)
+
+	expvar.NewString("version").Set(version)
+
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
